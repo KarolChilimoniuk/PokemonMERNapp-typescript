f4bfa95b15793777935cd55759c41179af776e85
Karol Chilimoniuk <webdevkarolchilimoniuk@gmail.com>
2022-09-24 18:47:46 +0200

    mod: client side index.js changed to index.tsx and updated to React 18 synthax

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,15 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
